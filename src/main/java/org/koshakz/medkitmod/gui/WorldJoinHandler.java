7a2a63896af809daf32a2649d3af046667301bac
Egor <yrce@ukr.net>
2025-04-08 21:09:05 +0300

    Big update

--- src/main/java/org/koshakz/medkitmod/gui/WorldJoinHandler.java
+++ src/main/java/org/koshakz/medkitmod/gui/WorldJoinHandler.java
@@ -13,23 +13,4 @@ import org.koshakz.medkitmod.gui.menu.VoMenu;
 @Mod.EventBusSubscriber(modid = "medkitmod", value = Dist.CLIENT)
 public class WorldJoinHandler {
 
-
-    @SubscribeEvent
-    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        // Проверяем только на клиенте и при завершении тика
-        if (event.phase != TickEvent.Phase.END || event.side != LogicalSide.CLIENT) return;
-
-        Player player = event.player;
-        Minecraft minecraft = Minecraft.getInstance();
-
-        // Проверяем что игрок - это мы, и мы в мире (не в меню)
-        if (player != minecraft.player || minecraft.level == null) {// Сброс при выходе из мира
-            return;
-        }
-
-        // Открываем меню один раз при входе в мир
-        if (minecraft.screen == null) {
-            minecraft.setScreen(new SelectTeamMenu());
-        }
-    }
 }
